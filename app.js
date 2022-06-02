139b7a911999829d8ffe710e084cd797b483575d
Aphenphos <MaxwillWinters@gmail.com>
2022-06-02 19:19:30 -0400

    added most of HTML and tested my text input

--- app.js
+++ app.js
@@ -5,3 +5,18 @@ const topTextDisplay = document.getElementById('top-text-display');
 const bottomText = document.getElementById('bottom-text');
 const bottomTextDisplay = document.getElementById('bottom-text-display');
 
+const imageSelector = document.getElementById('image-selector');
+const selectedImage = document.getElementById('selected-image');
+
+const backgroundSelector = document.getElementById('background-selector');
+const selectedBackground = document.getElementById('selected-background');
+
+topText.addEventListener('input', () => {
+    console.log('text changing');
+    topTextDisplay.textContent = topText.value;
+});
+
+bottomText.addEventListener('input', () => {
+    console.log('text changing');
+    bottomTextDisplay.textContent = bottomText.value;
+});
